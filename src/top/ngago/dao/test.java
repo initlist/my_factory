98bae1d0db0a26684f90fd90d6cb814a9c763a35
initlist <zhengngago@gmail.com>
2020-11-25 14:57:44 +0800

    搭建的基础环境

---
+++ src/top/ngago/dao/test.java
@@ -0,0 +1,22 @@
+package top.ngago.dao;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class test {
+    public static void main(String[] args) throws Exception {
+        Properties pro = new Properties();
+        InputStream is = DruidDataSource.class.getClassLoader().getResourceAsStream("druid.properties");
+        pro.load(is);
+        DataSource ds = DruidDataSourceFactory.createDataSource(pro);
+        Connection conn = ds.getConnection();
+        System.out.println(conn);
+
+    }
+}
